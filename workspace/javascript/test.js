6d2d771029955d22d292d3179ee578b35a23ce8f
Jairfsj <jairfsj@gmail.com>
2022-01-15 16:34:44 -0300

    modified : scripts

--- workspace/javascript/test.js
+++ workspace/javascript/test.js
@@ -3,8 +3,9 @@ function returnEvenValues(array) {
   for (let i= 0; i < array.length; i++){
       if (array[i] % 2 == 0){
           evenNums.push(array[i]);
-      
-    }
+      } else {
+        console.log(`${array[i]} não é par!`)
+      }
   
  }
  console.log('Os números pares são:' ,evenNums);
