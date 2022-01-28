1ccb784afd443e56744d74b52c344f3b1a29cb8f
Jairfsj <jairfsj@gmail.com>
2022-01-28 20:30:56 -0300

    new :  js-scripts

--- workspace/javascript/assets/js/scripts.js
+++ workspace/javascript/assets/js/scripts.js
@@ -1,1 +1,18 @@
-console.log("hello, world !");
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 0;
+
+function increment(){
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+
+}
+  
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 0;
+
+function decrement(){
+  curretNumber = currentNumber - 1;
+  currentNumberWrapper.innerHTML = currentNumber;
+}
+  
+
