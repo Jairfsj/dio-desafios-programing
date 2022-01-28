d12814d6ab4ab35b325bb6eda4e42d6f93346148
Jairfsj <jairfsj@gmail.com>
2022-01-28 20:39:43 -0300

    new :  js-scripts

--- workspace/javascript/assets/js/scripts.js
+++ workspace/javascript/assets/js/scripts.js
@@ -11,7 +11,7 @@ var currentNumberWrapper = document.getElementById("currentNumber");
 var currentNumber = 0;
 
 function decrement(){
-  curretNumber = currentNumber - 1;
+  currentNumber = currentNumber - 1;
   currentNumberWrapper.innerHTML = currentNumber;
 }
   
