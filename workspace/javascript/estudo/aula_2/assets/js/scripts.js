01e6a283ef4d704c469fa02e23982f92e859c83e
Jairfsj <jairfsj@gmail.com>
2022-02-05 18:37:59 -0300

    new : file

---
+++ workspace/javascript/estudo/aula_2/assets/js/scripts.js
@@ -0,0 +1,106 @@
+//Primitive Types
+
+//Booleans
+ 
+/*var vOrF = false;
+
+console.log(typeof(vOrF));
+
+// numbers
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+// strings
+var palavra = 'hoje';
+console.log(typeof(palavra));
+
+// function   
+var funcao = function(){}
+console.log(tyoeof(funcao));*/ 
+
+// Var Declaration
+/*var variable = 'Jair';
+variable = 'francisco';
+console.log(variable);
+
+let variable2 = 'jair';
+variable2 = 'Francisco'
+console.log(variable2);*/ 
+
+/*const constante = 'Jair ';
+constante = 'Francisco'
+console.log(constante);
+
+var escopoGlobal = 'global';
+console.log(escopoGlobal);
+
+function escopoLocal(){
+    let escopoLocalInterno = 'local';
+  console.log(escopoLocalInterno);
+
+}
+escopoLocal();
+*/ 
+
+//Atribuição 
+/*var atribuicao = 'Jair';
+
+//Comparação 
+var comparacao = '0' == 0;
+console.log(comparacao);
+
+//Comporação Identica 
+var comparacaoIdentica = '0' === 0 ;
+console.log(comparacaoIdentica);*/ 
+
+//Adição
+/*var adicao = 1 + 1;
+console.log(adicao);
+
+//Subitração 
+var subitracao = 3 - 1;
+console.log(subitracao);
+
+//Multiplicação 
+var multiplicacao = 4 * 5;
+console.log(multiplicacao);
+
+
+//Divisão Real 
+var divisaoreal = 12 / 4;
+console.log(divisaoreal);
+
+//Divisão inteira
+var divisaointeira = 6 % 4;
+console.log(divisaointeira);
+
+//Exponeação 
+var exponeacao = 12 ** 2;
+console.log(exponeacao);*/ 
+
+//Maior >
+/*var maiorque = 5 > 2;
+console.log(maiorque);
+
+//Menor <
+var menorque = 4 < 9
+console.log(menorque);
+
+// Maior ou igual
+var maiorOuIgual = 4 >= 4;
+console.log(maiorOuIgual);
+
+//Menor ou igual
+var menorOuIgual =  4 <= 4;
+console.log(menorOuIgual);*/ 
+
+//Operadores logicos
+var  e = true && false ;
+console.log(e);
+
+var ou = true || false;
+console.log(ou);
+
+var nao = !true;
+console.log(nao);
+
