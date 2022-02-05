5af9ee75f092da2ab334185ffe84ab7d5ec04a9c
Jairfsj <jairfsj@gmail.com>
2022-02-05 00:01:08 -0300

    array : new scripts

--- workspace/javascript/estudo-3/aula_3/assets/js/scripts.js
+++ workspace/javascript/estudo-3/aula_3/assets/js/scripts.js
@@ -1,6 +1,9 @@
 //O que são vetores ou array
 
 //Como declarar um array
-let array = ['string, 1, true'];
+/*let array = ['string, 1, true'];
 
-console.log(array);
+console.log(array);*/
+
+let array = ['string', 1, true,[array = [1],['array2']['array3']['array4']]];
+console.log()
