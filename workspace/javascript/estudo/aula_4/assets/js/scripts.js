b8cd2c83b4fac739ed90e73bbf83b3f00f348021
Jairfsj <jairfsj@gmail.com>
2022-02-07 21:14:17 -0300

    new : scripts

--- workspace/javascript/estudo/aula_4/assets/js/scripts.js
+++ workspace/javascript/estudo/aula_4/assets/js/scripts.js
@@ -1,13 +1,12 @@
 var gamer1 = -1;
 var gamer2 = 0;
 var placar;
-
-if (gamer1 != -1) {
-   if(gamer1 > 0) {
-      console.log('Gamer 1 marcou ponto!');
-   } else if (gamer2 > 0 ) {
-      console.log('Gamer2 marcou ponto!')
-   } else {
-      console.log('Ninguêm marcou ponto!');
-   }
+gamer1 != -1 &&  gamer2 != -1 ? console.log('Os gamers são validos') : console.log('Gamers Invalidos')
+if(gamer1 > 0) {
+    console.log('Gamer 1 marcou ponto!');
+} else if (gamer2 > 0 ) {
+    console.log('Gamer2 marcou ponto!')
+} else {
+    console.log('Ninguêm marcou ponto!');
 }
+
