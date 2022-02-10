834f118a83884e4d0d7315cb07f2df7c853768db
Jairfsj <jairfsj@gmail.com>
2022-02-09 23:20:17 -0300

    new : scripts

--- workspace/javascript/estudo/aula_4/assets/js/scripts.js
+++ workspace/javascript/estudo/aula_4/assets/js/scripts.js
@@ -4,34 +4,34 @@ var placar;
 
 
 //if ternário
-gamer1 != -1 &&  gamer2 != -1 ? console.log('Os gamers são validos') : console.log('Gamers Invalidos');
+gamer1 != -1 &&  gamer2 != -1 ? console.log('Os gamers são validos !') : console.log('Os gamers são invalidos !');
 
 //If
 if(gamer1 > 0 && gamer2 == 0) {
-    console.log('Gamer 1 marcou ponto!');
+    console.log('Gamer 1 marcou ponto !');
     placar = gamer1 > gamer2;
 } 
 
 //ElseIf
 else if (gamer2 > 0 && gamer1 == 0 ) {
-    console.log('Gamer 2 marcou ponto!');
+    console.log('Gamer 2 marcou ponto !');
     placar = gamer2 > gamer2;
 }
 
 //Else
 else {
-    console.log('Ninguêm marcou ponto!');
+    console.log('Ninguêm marcou ponto !');
 }
 
 //switch case
 switch (placar) {
   case placar = gamer1 > gamer2:
-       console.log('Gamer 1 ganhou a partida');  
+       console.log('Gamer 1 ganhou a partida !');  
     break;
   case placar = gamer2 > gamer1:
-       console.log('Gamer 2 ganhou a partida');
+       console.log('Gamer 2 ganhou a partida !');
     break;
   default:
-       console.log('Ninguém ganhou a partida')
+       console.log('Ninguém ganhou a partida !')
     break;
 }
