9e65e395da265129f3b29011123559b71e88570b
Jairfsj <jairfsj@gmail.com>
2022-02-09 22:54:26 -0300

    new : scripts

--- workspace/javascript/estudo/aula_4/assets/js/scripts.js
+++ workspace/javascript/estudo/aula_4/assets/js/scripts.js
@@ -1,12 +1,25 @@
-var gamer1 = -1;
+var gamer1 = 1;
 var gamer2 = 0;
 var placar;
-gamer1 != -1 &&  gamer2 != -1 ? console.log('Os gamers são validos') : console.log('Gamers Invalidos')
-if(gamer1 > 0) {
+
+
+//if ternário
+gamer1 != -1 &&  gamer2 != -1 ? console.log('Os gamers são validos') : console.log('Gamers Invalidos');
+
+//If
+if(gamer1 > 0 && gamer2 == 0) {
     console.log('Gamer 1 marcou ponto!');
-} else if (gamer2 > 0 ) {
-    console.log('Gamer2 marcou ponto!')
-} else {
+    placar = gamer1 > gamer2;
+} 
+
+//ElseIf
+else if (gamer2 > 0 && gamer1 == 0 ) {
+    console.log('Gamer2 marcou ponto!');
+    placar = gamer2 > gamer2;
+}
+
+//Else
+else {
     console.log('Ninguêm marcou ponto!');
 }
 
