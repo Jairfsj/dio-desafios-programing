13eb743d8fbd29b6adedf5023419c621ac35deb9
Jairfsj <jairfsj@gmail.com>
2022-02-12 15:17:27 -0300

    new : scripts

--- workspace/javascript/estudo/aula_6/assets/js/scripts.js
+++ workspace/javascript/estudo/aula_6/assets/js/scripts.js
@@ -1,5 +1,20 @@
-function funcao() {
+//Tipos de funtions
+
+//Declarativas
+/*function funcao() {
   console.log('Está é uma mensagem de um função declarativa (42)!')
+}  
+
+funcao();*/ 
+
+//Expressão de função 
+//Com nomeação
+
+var funcao = function funcao(){
+  console.log('Mensagem executada apartir de uma função de expressão!')
 }
+
 funcao();
+
+
 
