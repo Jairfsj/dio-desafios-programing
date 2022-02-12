6d3531c705154fb150ceebca0b49add9a8e5c1c2
Jairfsj <jairfsj@gmail.com>
2022-02-12 18:10:41 -0300

    new : scripts

--- workspace/javascript/estudo/calculator/assets/js/scripts.js
+++ workspace/javascript/estudo/calculator/assets/js/scripts.js
@@ -1,22 +1,16 @@
+ // criando uma calculadora
+
 function calculadora() {
-    const operacao = prompt('Escolha um operação:\n 1 - Soma (+)\n 2 - Subtração (-)\n 3 - Multiplicação (*)\n 4 - Divisão real (/)\n 5 - Divisão Inteira(%)\n 6 - Potenciação (**)');
-
-    let n1 = Number(prompt['Insira um numero: ']);
-    let n2 = Number(prompt['Insira outro numero: ']);
-
-
-
-    if (operacao == 1){
-      soma();
-    } else if (operacao == 2){
-      subtracao();
-    } else if (operacao == 3)
-      multiplicacao();
-    } else if (operacao == 4){
-      divisaoReal();
-    } else if (operacao == 5){
-      divisaoInteira();
-    } else if (operacao == 6){
-      potenciacao();
-    }
-    calculadora();
+   
+  // definindo as operações e pegando valor de entrada do usuário
+  
+  // usamos "\n" para fazer uma quebra de linha
+   
+  const operacao = prompt('Escolha uma opção:\n 1 - soma (+)\n 2 - subtração (-)\n 3 - multiplicação (*)\n 4 - divisão real (/)\n 5 - divisão inteira (%)\n 6 - potenciação (**)');
+  
+        // definindo variáveis
+        // todas as entradas devem ser do tipo number
+        let n1 = Number(prompt('Insira o primeiro valor:'));
+        let n2 = Number(prompt('Insira o segundo valor:'));
+        let resultado;
+calculadora();
