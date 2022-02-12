03ceca035c7649b804647496bd83faba7cb1fd66
Jairfsj <jairfsj@gmail.com>
2022-02-12 16:15:03 -0300

    new : scripts

--- workspace/javascript/estudo/calculator/assets/js/scripts.js
+++ workspace/javascript/estudo/calculator/assets/js/scripts.js
@@ -1,1 +1,7 @@
-console.log();
+function calculadora() {
+  const operacao = prompt('Escolha um operação:\n 1 - Soma (+)\n 2 - Subtração (-)\n - 3 - Multiplicação (*)\n 4 - Divisão real (/)\n 5 - Divisão Inteira(%)\n 6 - Potenciação (**)');
+
+  console.log(operacao);
+} 
+
+calculadora();
