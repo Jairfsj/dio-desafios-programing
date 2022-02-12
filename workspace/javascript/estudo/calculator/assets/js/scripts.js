7a1bf43268327eb80be3e0c15cf899853dabcc3f
Jairfsj <jairfsj@gmail.com>
2022-02-12 18:27:09 -0300

    new : scripts

--- workspace/javascript/estudo/calculator/assets/js/scripts.js
+++ workspace/javascript/estudo/calculator/assets/js/scripts.js
@@ -13,4 +13,24 @@ function calculadora() {
         let n1 = Number(prompt('Insira o primeiro valor:'));
         let n2 = Number(prompt('Insira o segundo valor:'));
         let resultado;
+  function soma() {
+    resultado = n1 + n2;
+    alert(`${n1} + ${n2} = ${resultado}`);
+    
+  }
+  if (operacao == 1) {
+      soma();
+  } else if (operacao == 2) {
+      subtracao();
+  } else if (operacao == 3) {
+      multiplicacao();
+  } else if (operacao == 4) {
+      divisaoInteira();
+  } else if (operacao == 5) {
+      divisaoReal();
+  } else if (operacao == potenciacao){
+      potenciacao();
+  }
+
+  }
 calculadora();
