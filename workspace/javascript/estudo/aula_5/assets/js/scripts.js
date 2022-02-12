543704080c6c39db956f76c585dd64da93414eb6
Jairfsj <jairfsj@gmail.com>
2022-02-12 14:24:49 -0300

    new : scripts

--- workspace/javascript/estudo/aula_5/assets/js/scripts.js
+++ workspace/javascript/estudo/aula_5/assets/js/scripts.js
@@ -1,1 +1,12 @@
-console.log();
+/*function texto() {
+  console.log('Este texto veio de uma função!')
+  
+}
+
+texto();*/ 
+
+function mensagem(primeiro, segundo) {
+  console.log(primeiro, segundo)
+}
+
+mensagem('Tudo';, 'Bem !');
