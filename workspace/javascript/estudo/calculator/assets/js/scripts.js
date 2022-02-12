516cb88bf51b8e244b586ccaebfe03bdb2e9942d
Jairfsj <jairfsj@gmail.com>
2022-02-12 17:31:12 -0300

    new : scripts

--- workspace/javascript/estudo/calculator/assets/js/scripts.js
+++ workspace/javascript/estudo/calculator/assets/js/scripts.js
@@ -1,19 +1,22 @@
 function calculadora() {
     const operacao = prompt('Escolha um operação:\n 1 - Soma (+)\n 2 - Subtração (-)\n 3 - Multiplicação (*)\n 4 - Divisão real (/)\n 5 - Divisão Inteira(%)\n 6 - Potenciação (**)');
 
-}
+    let n1 = Number(prompt['Insira um numero: ']);
+    let n2 = Number(prompt['Insira outro numero: ']);
 
-if (operacao == 1){
-    soma();
-} else if (operacao == 2){
-    subtracao();
-} else if (operacao == 3)
-    multiplicacao();
-} else if (operacao == 4){
-    divisaoReal();
-} else if (operacao == 5){
-    divisaoInteira();
-} else if (operacao == 6){
-    potenciacao();
-}
-calculadora();
+
+
+    if (operacao == 1){
+      soma();
+    } else if (operacao == 2){
+      subtracao();
+    } else if (operacao == 3)
+      multiplicacao();
+    } else if (operacao == 4){
+      divisaoReal();
+    } else if (operacao == 5){
+      divisaoInteira();
+    } else if (operacao == 6){
+      potenciacao();
+    }
+    calculadora();
