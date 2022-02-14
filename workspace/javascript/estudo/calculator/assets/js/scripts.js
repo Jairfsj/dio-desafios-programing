3c3dfa7e527833acc36af4aee9af2b3d70cbe7a7
Jairfsj <jairfsj@gmail.com>
2022-02-13 22:49:07 -0300

    new : scripts

--- workspace/javascript/estudo/calculator/assets/js/scripts.js
+++ workspace/javascript/estudo/calculator/assets/js/scripts.js
@@ -14,34 +14,52 @@ function calculadora() {
         let n2 = Number(prompt('Insira o segundo valor:'));
         let resultado;
   function soma() {
-    resultado = n1 + n2;
-    alert(`${n1} + ${n2} = ${resultado}`)
+     resultado = n1 + n2;
+     alert(`${n1} + ${n2} = ${resultado}`)
+     novaOperacao();
   }
   
   function subtracao() {
-    resultado = n1 - n2;
-    alert(`${n1} - ${n2} = ${resultado}`)
+     resultado = n1 - n2;
+     alert(`${n1} - ${n2} = ${resultado}`)
+     novaOperacao();
   }
   
   function multiplicacao() {
-    resultado = n1 * n2;
-    alert(`${n1} * ${n2} = ${resultado}`)
+     resultado = n1 * n2;
+     alert(`${n1} * ${n2} = ${resultado}`)
+     novaOperacao();
   }
   
   function divisaoReal() {
-    resultado = n1 / n2;
-    alert(`${n1} / ${n2} = ${resultado}`)
+     resultado = n1 / n2;
+     alert(`${n1} / ${n2} = ${resultado}`)
+     novaOperacao();
   }
   
   function divisaoInteira() {
-    resultado = n1 % n2;
-    alert(`O resto da divisão entre ${n1} e ${n2} é igual a ${resultado}`)
+     resultado = n1 % n2;
+     alert(`O resto da divisão entre ${n1} e ${n2} é igual a ${resultado}`)
+     novaOperacao();
   }
   function potenciacao() {
      resultado = n1 ** n2;
-     alert(`${n1} elevado à ${n2}ª potência é ${resultado}`);
+     alert(`${n1} elevado à ${n2}ª potência é ${resultado}`)
+     novaOperacao();
   }
   
+  function novaOperacao() {
+     let opcao = prompt('Você deseja fazer outra conta?'\n 1 - Sim\n 2 - Não');
+    
+     if (opcao == 1 ){
+         calculadora();
+     } else id (opcao == 2){
+       alert('Até logo!')
+     } else {
+       alert('Digite uma opção dentre as disponivéis!')
+       novaOperacao();
+     }
+  } 
   if (operacao == 1) {
       soma();
   } else if (operacao == 2) {
