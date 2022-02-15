a21bd94cb2a31e916b2cce14e28cb31f145bad1a
Jairfsj <jairfsj@gmail.com>
2022-02-15 18:56:42 -0300

    new : scripts

--- workspace/javascript/estudo/calculator/assets/js/scripts.js
+++ workspace/javascript/estudo/calculator/assets/js/scripts.js
@@ -8,59 +8,65 @@ function calculadora() {
    
   const operacao = prompt('Escolha uma opção:\n 1 - soma (+)\n 2 - subtração (-)\n 3 - multiplicação (*)\n 4 - divisão real (/)\n 5 - divisão inteira (%)\n 6 - potenciação (**)');
   
-        // definindo variáveis
-        // todas as entradas devem ser do tipo number
-        let n1 = Number(prompt('Insira o primeiro valor:'));
-        let n2 = Number(prompt('Insira o segundo valor:'));
-        let resultado;
-  function soma() {
-     resultado = n1 + n2;
-     alert(`${n1} + ${n2} = ${resultado}`)
-     novaOperacao()
-  }
+  if (!operacao || operacao > 7) {
+      alert ('Erro - operação inesistente!');
+      calculadora();
+  } else {
+            // definindo variáveis
+            // todas as entradas devem ser do tipo number
+              let n1 = Number(prompt('Insira o primeiro valor:'));
+              let n2 = Number(prompt('Insira o segundo valor:'));
+              let resultado;
+              
+              function soma() {
+                  resultado = n1 + n2;
+                  alert(`${n1} + ${n2} = ${resultado}`)
+                  novaOperacao()
+                  }
   
-  function subtracao() {
-     resultado = n1 - n2;
-     alert(`${n1} - ${n2} = ${resultado}`)
-     novaOperacao()
-  }
+              function subtracao() {
+                  resultado = n1 - n2;
+                  alert(`${n1} - ${n2} = ${resultado}`)
+                  novaOperacao()
+                  }
   
-  function multiplicacao() {
-     resultado = n1 * n2;
-     alert(`${n1} * ${n2} = ${resultado}`)
-     novaOperacao()
-  }
+              function multiplicacao() {
+                  resultado = n1 * n2;
+                  alert(`${n1} * ${n2} = ${resultado}`)
+                  novaOperacao()
+                  }
   
-  function divisaoReal() {
-     resultado = n1 / n2;
-     alert(`${n1} / ${n2} = ${resultado}`)
-     novaOperacao()
-  }
+              function divisaoReal() {
+                  resultado = n1 / n2;
+                  alert(`${n1} / ${n2} = ${resultado}`)
+                  novaOperacao()
+                  }
   
-  function divisaoInteira() {
-     resultado = n1 % n2;
-     alert(`O resto da divisão entre ${n1} e ${n2} é igual a ${resultado}`)
-     novaOperacao()
-  }
-  function potenciacao() {
-     resultado = n1 ** n2;
-     alert(`${n1} elevado à ${n2}ª potência é ${resultado}`)
-     novaOperacao()
-  }
+              function divisaoInteira() {
+                  resultado = n1 % n2;
+                  alert(`O resto da divisão entre ${n1} e ${n2} é igual a ${resultado}`)
+                  novaOperacao()
+                  }
+              function potenciacao() {
+                  resultado = n1 ** n2;
+                  alert(`${n1} elevado à ${n2}ª potência é ${resultado}`)
+                  novaOperacao()
+                  }
   
-  function novaOperacao() {
-     let opcao = prompt('Você deseja fazer outra conta?\n 1 - Sim\n 2 - Não');
+              function novaOperacao() {
+                let opcao = prompt('Você deseja fazer outra conta?\n 1 - Sim\n 2 - Não');
     
-     if (opcao == 1) {
-         calculadora();
-     } else if (opcao == 2) {
-          alert('Até logo!');
-     } else {
-          alert('Digite uma opção dentre as disponivéis!')
-          novaOperacao();
-     }
+                if (opcao == 1) {
+                  calculadora();
+                } else if (opcao == 2) {
+                  alert('Até logo!');
+                } else {
+                  alert('Digite uma opção dentre as disponivéis!')
+                  novaOperacao();
+                }
+  
+          }
   }
-
   if (operacao == 1) {
       soma();
   } else if (operacao == 2) {
