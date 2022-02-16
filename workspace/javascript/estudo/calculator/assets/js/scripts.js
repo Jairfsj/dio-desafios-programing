a48c119c61b2a0f4383499bfc2229bebc05a5dd6
Jairfsj <jairfsj@gmail.com>
2022-02-16 20:07:38 -0300

    new : scripts

--- workspace/javascript/estudo/calculator/assets/js/scripts.js
+++ workspace/javascript/estudo/calculator/assets/js/scripts.js
@@ -4,7 +4,6 @@ function calculadora() {
    
   // definindo as operações e pegando valor de entrada do usuário
   
-  // usamos "\n" para fazer uma quebra de linha
    
   const operacao = prompt('Escolha uma opção:\n 1 - soma (+)\n 2 - subtração (-)\n 3 - multiplicação (*)\n 4 - divisão real (/)\n 5 - divisão inteira (%)\n 6 - potenciação (**)');
   
